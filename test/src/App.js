f9912f35eea3cfdd8ebaf5ee8a5c4b8ec4a1144d
franswijaya <franswijaya491@gmail.com>
2022-10-14 01:27:00 +0700

    third commit

--- test/src/App.js
+++ test/src/App.js
@@ -1,5 +1,13 @@
 import './App.css';
 import Post from './Post';
+import {
+  SearchIcon,
+  PaperAirplaneIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon
+} from '@heroicons/react/outline';
+import { HomeIcon } from "@heroicons/react/solid";
 
 function App() {
   return (
@@ -11,9 +19,31 @@ function App() {
         alt=""
         />
 
-        <div>
-          <button onClick> Login </button>
-          <button onClick> Sign Up </button>
+        <div className="mas-w-xs">
+          <div className="relative mt-1 p-3 rounded-md">
+            <div className="absolute insert-y-0 pl-3 flex items-center-pointer-events-none">
+              <SearchIcon className="h-5 w-5 text-gray-500"/>
+            </div>
+            <input 
+            className="bg-gray-50 block w-full pl-10 sm:text-smborder-gray-300 focus:ring-black focus:border-black rounded-md"
+            type="text" 
+            placeholder="Search"
+            />
+          </div>
+        </div>
+        
+        <div className="flex item-center justify-end space-x-4">
+          <HomeIcon className="h-7 w-7 cursor-pointer hover:scale-125 transition-all duration-150 ease-out"/>
+          <PaperAirplaneIcon className="h-7 w-7 cursor-pointer hover:scale-125 transition-all duration-150 ease-out"/>
+          <PlusCircleIcon className="h-7 w-7 cursor-pointer hover:scale-125 transition-all duration-150 ease-out"/>
+          <UserGroupIcon className="h-7 w-7 cursor-pointer hover:scale-125 transition-all duration-150 ease-out"/>
+          <HeartIcon className="h-7 w-7 cursor-pointer hover:scale-125 transition-all duration-150 ease-out"/>
+
+          <img
+            src="https://expertphotography.b-cdn.net/wp-content/uploads/2018/10/cool-profile-pictures-retouching-1.jpg"
+            alt="profile pic"
+            className="h-10 w-10 rounded-full cursor-pointer"
+          />
         </div>
 
       </div>
@@ -21,7 +51,6 @@ function App() {
       
      
       <Post username="Hana" caption="Feeling Good with The Sun" imageUrl="https://expertphotography.b-cdn.net/wp-content/uploads/2018/10/cool-profile-pictures-retouching-1.jpg"/>
-      <Post username="Maul" caption="XXX" imageUrl=""/>
       
     </div>
   );
