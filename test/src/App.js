a39c6a4dfacd536cd40f5ad00fec5c1fec29f00c
franswijaya <franswijaya491@gmail.com>
2022-10-13 12:29:10 +0700

    third commit

--- test/src/App.js
+++ test/src/App.js
@@ -13,6 +13,7 @@ function App() {
       </div>
 
       <Post username="Hana" caption="Feeling Good with The Sun" imageUrl="https://expertphotography.b-cdn.net/wp-content/uploads/2018/10/cool-profile-pictures-retouching-1.jpg"/>
+      <Post username="Maul" caption="XXX" imageUrl=""/>
       
     </div>
   );
--- test/src/Post.js
+++ test/src/Post.js
@@ -4,17 +4,17 @@ import Avatar from "@material-ui/core/Avatar";
 
 function Post({ username, caption, imageUrl }) {
   return (
-    <div classname="post">
-      <div classname="post_header">
+    <div className="post">
+      <div className="post_header">
         <Avatar
-          classname="post_avatar"
+          className="post_avatar"
           alt='Hana'
           src="/static/images/avatar/1.jpg"
         />
         <h3>{username}</h3>
       </div>
 
-      <img classname="post_image" src="https://expertphotography.b-cdn.net/wp-content/uploads/2018/10/cool-profile-pictures-retouching-1.jpg" alt=""/>
+      <img className="post_image" src="https://expertphotography.b-cdn.net/wp-content/uploads/2018/10/cool-profile-pictures-retouching-1.jpg" alt=""/>
 
       <h4 className="post_text"><strong>{username}</strong> {caption}</h4>
     </div>
